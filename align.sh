8a22dfbf852eb2b3b2a19157bb248fe1ba9a0d31
Jay Dellinger <jay@dellingertechnologies.com>
2012-03-22 23:09:51 -0400

    Added screenshots and files for signing/publishing app

---
+++ align.sh
@@ -0,0 +1,1 @@
+zipalign -f -v 4 target/JavaJukebox.apk target/JavaJukebox-aligned.apk && mv target/JavaJukebox-aligned.apk target/JavaJukebox.apk
---
+++ sign.sh
@@ -0,0 +1,1 @@
+jarsigner -keystore javajukebox.keystore target/JavaJukebox.apk javajukebox
